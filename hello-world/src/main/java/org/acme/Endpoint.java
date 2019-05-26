658f1dfe73c46a9081074d202b93967eaa390c83
Ioannis Canellos <iocanel@gmail.com>
2019-05-26 10:31:37 +0300

    feat: Add service and config property.

--- hello-world/src/main/java/org/acme/Endpoint.java
+++ hello-world/src/main/java/org/acme/Endpoint.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +9,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class Endpoint {
 
+    @Inject
+    GreetingService greetingService;
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return greetingService.greet();
     }
 }
---
+++ hello-world/src/main/java/org/acme/GreetingService.java
@@ -0,0 +1,17 @@
+
+package org.acme;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @ConfigProperty(name="message")
+    String message;
+    
+    String greet() {
+        return message;
+    }
+}
